7312e54a0e1fc8127922d2d62498c0668d41af32
ceciliabolanos <chechubolanos@gmail.com>
2024-05-06 16:33:46 +0000

    more results

--- run_CKA.sh
+++ run_CKA.sh
@@ -1,16 +1,15 @@
-'''
-python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_base' --layer1 10 --model2 'encodecmae_mel256-ec-base' --layer2 10
 
-python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base' --layer2 10
-'''
+#python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'bert-base-uncased' --layer1 12 --model2 'encodecmae_mel256-ec-base' --layer2 10
+
 
 python3 experiments/experiment_CKA.py --model1 'wav2vec2' --layer1 12 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
 python3 experiments/experiment_CKA.py --model1 'wav2vec2' --layer1 12 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
@@ -18,13 +17,13 @@ python3 experiments/experiment_CKA.py --model1 'wav2vec2' --layer1 12 --model2 '
 python3 experiments/experiment_CKA.py --model1 'wav2vec2' --layer1 12 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
 python3 experiments/experiment_CKA.py --model1 'wav2vec2' --layer1 12 --model2 'encodecmae_mel256-ec-base' --layer2 10
 
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base_st-nopn' --layer1 10 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base_st-nopn' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base_st-nopn' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base-as' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base-as' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
-python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base-fma' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base_st-nopn' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base_st-nopn' --layer1 10 --model2 'encodecmae_mel256-ec-base-as' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base_st-nopn' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base_st-nopn' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base-as' --layer1 10 --model2 'encodecmae_mel256-ec-base-fma' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base-as' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
+#python3 experiments/experiment_CKA.py --model1 'encodecmae_mel256-ec-base-fma' --layer1 10 --model2 'encodecmae_mel256-ec-base-ll' --layer2 10
